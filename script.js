409910617d0db3b2cafbc6a68edc0bf9373b1457
estherpinos <estherpinos1@gmail.com>
2023-09-08 15:39:43 +0200

    Add [codice, costo, e display none-block]

--- script.js
+++ script.js
@@ -1,41 +1,65 @@
 
+// ANNULLA
+document.getElementById('annulla').addEventListener('click', function(){
 
+  document.querySelector('.biglietto').classList.add('d-none');
+})
 
 document.getElementById('genera').addEventListener('click', function(){
 
-  // PRICES
-  // let price = 0.21 * kilometers; 
-  // let discountChild = price * 0.2;
-  // let discountAdult = price * 0.4;
-  // if (age<18) price = price - discountChild;
-  // if (age>65) price = price - discountAdult;
+  document.querySelector('.biglietto').classList.remove('d-none');
 
   // CONSTS
 
   const nome = document.getElementById('nome').value;
   const km = document.getElementById('km').value;
+  //CONTROLLO KM
+  if(isNaN(km)){
+    alert('Il campo km deve essere un numero');
+    document.querySelector('.biglietto').classList.add('d-none');
+
+  }
   const options = document.getElementById('inputGroupSelect01').selectedIndex;
-  console.log(nome, km, options);
+  
 
   // NOME PASSEGGERO
   document.getElementById('nome-passeggero').innerHTML = nome;
 
-  // OFFERTA
+  // OFFERTA E CALCOLO PREZZO
+  let price = 0.21 * km; 
+
+  if(options == 0){
+    alert('Scegliere una fascia d\'età');
+    document.querySelector('.biglietto').classList.add('d-none');
+  }
+
   if(options == 1){
     document.getElementById('offerta').innerHTML = 'Biglietto ridotto';
+    price = price * 0.8;
   }
   if(options == 2){
     document.getElementById('offerta').innerHTML = 'Biglietto Standard';
   }
   if(options == 3){
     document.getElementById('offerta').innerHTML = 'Biglietto Silver';
+    price = price * 0.6;
   }
 
   // CARROZZA
   const numero = Math.floor(Math.random() *20)+1;
-  console.log(numero);
 
   document.getElementById('carrozza').innerHTML = numero;
+
+  // CODICE
+   const codice = Math.floor(Math.random() *99000)+10000;
+
+   document.getElementById('codice').innerHTML = codice;
+
+  // COSTO
+
+  document.getElementById('costo').innerHTML = price.toFixed(2)+'€';
+
+
 
 
 
